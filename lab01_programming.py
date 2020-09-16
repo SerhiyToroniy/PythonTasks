099d099a9f4917dc3bb375a936ac98d651a8e1db
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2020-09-16 16:31:44 +0300

    Add comment to the code

--- lab01_programming.py
+++ lab01_programming.py
@@ -1,32 +1,32 @@
 def k_nums(n):
-    print(1)  # exception
-    i = 0
-    count = 0
-    while True:
-        if count == n:
-            return
-        my_pow = i * i
-        P = str(my_pow)
-        a = b = ""
-        z = k = 1
-        for j in P:
-            a += j
-            if a == P:
-                continue
-            for j in P:
-                if z > 0:
-                    z -= 1
+     if n <= 0:
+          return
+     print(1) #exception
+     i = 45 #iterator
+     count = 1 # counre for first Kaprekar's nums
+     while True:
+          if count == n:
+               return
+          my_pow = i * i
+          P = str(my_pow)
+          a = b ="" #a - left part / b - right part
+          z = k = 1 #z ignore left part for correct creatting right part / k remember left part's len
+          for j in P:
+               a += j #add symbols from right to left
+               if a == P:
                     continue
-                b += j
-            k += 1
-            z = k
-            int_a = int(a)
-            int_b = int(b)
-            if int_a + int_b == i and int_a != 0 and int_b != 0:
-                print(i)
-                count += 1
-            b = ""
-        i += 1
-
-
-k_nums(int(input("Input N: ")))
+               for h in P:
+                    if z > 0: #z is used for the independece of k
+                         z -= 1
+                         continue
+                    b += h
+               k += 1
+               z = k
+               int_a = int(a) #str -> int
+               int_b = int(b)
+               if int_a + int_b == i and int_a != 0 and int_b != 0:
+                         print(i)
+                         count += 1
+               b = "" #update right part
+          i += 1
+k_nums(int(input("Input N: "))) #call the function
