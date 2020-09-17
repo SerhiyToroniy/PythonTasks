dbafc0ec21185873ecb5ff2ca4627e916550c569
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2020-09-17 19:11:41 +0300

    Validate input data

--- lab01_educational_practice.py
+++ lab01_educational_practice.py
@@ -10,12 +10,12 @@ def transfer_to_binary(x): #the function transfer decimal nums to binary
 def count_nums(N, K):    #the function counts suitable nums
     i = 1 #a condition from the task
     zero_count = global_count = 0 #global_count counts suitable nums
-    while i != N:
+    while i != int(N):
         bin_num = transfer_to_binary(i) #call the function
         for j in bin_num: # counts zero in binary num
             if j == "0":
                 zero_count += 1
-        if zero_count == K: #compare zero_count and input data
+        if zero_count == int(K): #compare zero_count and input data
             global_count += 1
         i += 1
         zero_count = 0 #update zero counter
@@ -23,7 +23,7 @@ def count_nums(N, K):    #the function counts suitable nums
 
 def condition_acsses(s):    #the function check correct input data
     k = n = "" #k - left number; n - right number
-    float_check = space_check = False
+    space_check = False
     for z in s:
         if z == " ":
             space_check = True
@@ -31,15 +31,9 @@ def condition_acsses(s):    #the function check correct input data
             n += z #add symbols to LEFT num (using concatenation)
         else:
             k += z #add symbols to RIGHT num (using concatenation)
-        if z == "." or z == ",": #check for double nums
-            float_check = True
-    if float_check:
-        print("N and K should be integer and lower than 109!")
-        return
-    a = int(n) #str -> int
-    b = int(k)
-    if a > 109 or b > 109 or a < 1 or b < 1:
-        print("N and K should be integer and lower than 109!")
-        return
-    count_nums(a, b)#call other function
+    if float(n) % 1 != 0 or float(k) % 1 != 0:
+        raise Exception("N and K should be integer!")
+    elif int(n) > 109 or int(k) > 109 or int(n) < 1 or int(k) < 1:
+        raise Exception("N and K should be natural numbers and lower than 109!")
+    count_nums(n, k)#call other function
 condition_acsses(input("Input N and K using \'SPACE\': "))
