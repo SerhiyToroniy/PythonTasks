0393e0ba5aeab1c84b6623add786bb9e6890e835
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2020-10-11 22:41:04 +0300

    Update SwiftTransfer.py
    Now a cycle is used in __str__. Setters are added.

--- SwiftTransfer.py
+++ SwiftTransfer.py
@@ -1,6 +1,7 @@
 class SwiftTransfer:
-    def __init__(self, id = None, iban_number = None, account_holder = None, amount = None, currency = None, fee_amount = None, payment_date = None):
-        self.id = id
+
+    def __init__(self, ident = None, iban_number = None, account_holder = None, amount = None, currency = None, fee_amount = None, payment_date = None):
+        self.ident = ident
         self.iban_number = iban_number
         self.account_holder = account_holder
         self.amount = amount
@@ -8,31 +9,40 @@ class SwiftTransfer:
         self.fee_amount = fee_amount
         self.payment_date = payment_date
 
-    def __str__(self):#possibility to use "print(SwiftTransfer)"
-        return "ID: " + str(self.id) + "\nIban_num: " + str(self.iban_number) + "\nAccount holder: " + str(self.account_holder) + "\nAmount: " + str(self.amount) + "\nCurrency: " + str(self.currency) + "\nFee_amount: " +str(self.fee_amount) + "\nPayment_date: " + str(self.payment_date)
+    def __str__(self):#possibility to use "print(SwiftTransfer())"
+        result = ""
+        for i in dir(self):
+            if i[0]+i[1]!="ge" and i[0]+i[1]!="se" and i[0]+i[1]!="__":   result+=i.upper() + ": " + str(getattr(self,i)) + "\n"
+        return result
 
-    def _input(self):
-        self.id = input("ID: ")
-        self.iban_number = input("Iban number: ")
-        self.account_holder = input("Account holder: ")
-        self.amount = input("Amount: ")
-        self.currency = input("Currency: ")
-        self.fee_amount = input("Fee amount: ")
-        self.payment_date = input("Payment date: ")
-        return self
-
-    #getters for every single attribute of out class
-    def _getID(self):
-        return self.id
-    def _getIBAN_NUMBER(self):
+    #getters for every single attribute of our class
+    def getID(self):
+        return self.ident
+    def getIBAN_NUMBER(self):
         return self.iban_number
-    def _getACCOUNT_HOLDER(self):
+    def getACCOUNT_HOLDER(self):
         return self.account_holder
-    def _getAMOUNT(self):
+    def getAMOUNT(self):
         return self.amount
-    def _getCURRENCY(self):
+    def getCURRENCY(self):
         return self.currency
-    def _getFEE_AMOUNT(self):
+    def getFEE_AMOUNT(self):
         return self.fee_amount
-    def _getPAYMENT_DATE(self):
+    def getPAYMENT_DATE(self):
         return self.payment_date
+
+    #setters for every single attribute of our class
+    def setID(self, var):
+        self.ident = var
+    def setIBAN_NUMBER(self, var):
+        self.iban_number = var
+    def setACCOUNT_HOLDER(self, var):
+        self.account_holder = var
+    def setAMOUNT(self, var):
+        self.amount = var
+    def setCURRENCY(self, var):
+        self.currency = var
+    def setFEE_AMOUNT(self, var):
+        self.fee_amount = var
+    def setPAYMENT_DATE(self, var):
+        self.payment_date = var
