7d6bc83455b231f5bd8b48ce2835727299904fff
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2020-10-25 19:16:58 +0200

    Decorators are used for validation now.

--- Validation.py
+++ Validation.py
@@ -1,4 +1,45 @@
+import os.path
+
 class Validation:
+
+    @staticmethod
+    def index_check(func):
+        def inner(lst, filename, index):
+            index = input("Index: ")
+            while not Validation().digit_check(index):
+                index = input("Index must be a positive number: ")
+            index = int(index)
+            while index != 0 and lst.length() == 0:
+                index = input("Your list is empty, you can choose only \"0\" index: ")
+                while not Validation().digit_check(index):
+                    index = input("Index must be a positive number: ")
+                index = int(index)
+            while index > lst.length() or index < 0:
+                index = input("Index must be in list range: ")
+                while not Validation().digit_check(index):
+                    index = input("Index must be a positive number: ")
+                index = int(index)
+            func(lst,filename, index)
+        return inner
+
+    @staticmethod
+    def file_check(func):
+        def inner(lst, filename, index):
+            filename = input("Input name of data file: ")
+            while not os.path.exists(filename):
+                filename = input("File doesn't exist: ")
+            func(lst, filename, index)
+        return inner
+
+    @staticmethod
+    def limit_check(func):
+        def inner(lst, limit, index):
+            limit = input("Input limit for iterator: ")
+            while not Validation().digit_check(limit):
+                limit = input("Limit must be a positive number: ")
+            func(lst, limit, index)
+        return inner
+
 
     @staticmethod
     def digit_check(var):
@@ -19,6 +60,11 @@ class Validation:
             for i in wo_minusdots:
                 if not i.isdigit():
                     return False
-            result = Validation().digit_check(wo_minusdots) and dot_count<=1
+            if minus_count > 0:
+                return var[0] == "-"
+            dot_position = True
+            if dot_count > 0:
+                dot_position = (var[0]!="." and var[len(var)-1]!=".")
+            result = Validation().digit_check(wo_minusdots) and dot_count<=1 and dot_position
         else: return False
         return result
