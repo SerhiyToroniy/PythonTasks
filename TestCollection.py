26ecb0b6d05330362fbf760e5a2d5c8b8b4a46f9
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2020-11-11 23:05:00 +0200

    Similar parts of code were deleted. Cycles were created

--- TestCollection.py
+++ TestCollection.py
@@ -32,74 +32,33 @@ class TestCollection(unittest.TestCase):
         self.assertIsInstance(temp[0], SwiftTransfer)
 
     def test_find(self):
-        self.assertEqual(self.arr.search("Tor"),[SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17")])
-        self.assertEqual(self.arr.search("Tor"),[SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17")])
+        self.assertEqual(self.arr.search("Tor"),[self.arr[0]])
+        self.assertEqual(self.arr.search("eur"),[self.arr[0],self.arr[1],self.arr[2]])
 
-
-        self.assertEqual(self.arr.search("seven"),[SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17")])
-        self.assertEqual(self.arr.search("77777"),[SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17")])
-        self.assertEqual(self.arr.search("UA77"),[SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17")])
-        self.assertEqual(self.arr.search("2020.07.17"),[SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17")])
-
-        self.assertEqual(self.arr.search("ivan"),[SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04")])
-        self.assertEqual(self.arr.search("67899"),[SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04")])
-        self.assertEqual(self.arr.search("8888"),[SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04")])
-        self.assertEqual(self.arr.search("2020.03.04"),[SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04")])
-
-        self.assertEqual(self.arr.search("eur"),[SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"),
-                                                 SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"),
-                                                 SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17")])
-
-        self.assertNotEqual(self.arr.search("tor"),[SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04")])
-        self.assertNotEqual(self.arr.search("7777"),[SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04")])
+        self.assertNotEqual(self.arr.search("tor"),[self.arr[1]])
+        self.assertNotEqual(self.arr.search("7777"),[self.arr[1]])
 
     def test_sort(self):
-        temp = Collection()
-        temp.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        temp.add(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"))
-        temp.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-
         unCorrect = Collection()
-        unCorrect.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        unCorrect.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-        unCorrect.add(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"))
+        for i in range(self.arr.len()):
+            unCorrect.add(self.arr[i])
 
         self.arr.sort("iban", self.arr)
-        self.assertEqual(self.arr,temp)
         self.arr.sort("fee", self.arr)
         self.assertEqual(self.arr,unCorrect)
+
         self.arr.sort("currency", self.arr)
         self.assertEqual(self.arr,unCorrect)
 
 
     def test_delete(self):
-        temp = Collection()
-        temp.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        temp.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-
-        unCorrect = Collection()
-        unCorrect.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        unCorrect.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-        unCorrect.add(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"))
-
         self.arr.delete_by_id("77777")
-        self.assertEqual(self.arr,temp)
         self.assertNotIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"), self.arr)
+
         self.arr.delete_by_id("67899")
         self.assertNotIn(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"), self.arr)
-        self.assertNotEqual(self.arr,unCorrect)
 
     def test_add(self):
-        temp = Collection()
-        temp.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        temp.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-        temp.add(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"))
-        temp.add(SwiftTransfer("55555", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"))
-
-        unCorrect = Collection()
-        unCorrect.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        unCorrect.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-
         self.arr.add_inputed(self.arr, SwiftTransfer("55555", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"))
         self.assertIn(SwiftTransfer("55555", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"), self.arr)
 
@@ -109,36 +68,16 @@ class TestCollection(unittest.TestCase):
         self.arr.add_inputed(self.arr, SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
         self.assertIn(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"), self.arr)
 
-        self.assertNotEqual(self.arr,unCorrect)
-
     def test_edit(self):
-        temp = Collection()
-        temp.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        temp.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-        temp.add(SwiftTransfer("77777", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"))
-
-        unCorrect = Collection()
-        unCorrect.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        unCorrect.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-
         self.arr.edit_by_id("77777", self.arr, SwiftTransfer("77777", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"))
-
         self.assertNotIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"),self.arr)
         self.assertIn(SwiftTransfer("77777", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"),self.arr)
 
-        self.assertEqual(self.arr,temp)
-        self.assertNotEqual(self.arr,unCorrect)
+        self.arr.edit_by_id("68292", self.arr, SwiftTransfer("68292", "UA22 2222 2222 2222", "Toroniy Ivan", "30000.00", "usd", "600.00", "2018.09.17"))
+        self.assertNotIn(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"),self.arr)
+        self.assertIn(SwiftTransfer("68292", "UA22 2222 2222 2222", "Toroniy Ivan", "30000.00", "usd", "600.00", "2018.09.17"),self.arr)
 
     def test_undo(self):
-        temp = Collection()
-        temp.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        temp.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-        temp.add(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"))
-
-        unCorrect = Collection()
-        unCorrect.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        unCorrect.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-
         caretaker = Caretaker(self.arr)
         caretaker.backup()
         self.arr.delete_by_id("77777")
@@ -149,19 +88,8 @@ class TestCollection(unittest.TestCase):
         caretaker.backup()
         caretaker.undo()
         self.assertIn(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"),self.arr)
-        self.assertEqual(self.arr, temp)
-        self.assertNotEqual(self.arr, unCorrect)
 
     def test_redo(self):
-        temp = Collection()
-        temp.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        temp.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-
-        unCorrect = Collection()
-        unCorrect.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        unCorrect.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
-        unCorrect.add(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"))
-
         caretaker = Caretaker(self.arr)
         caretaker.backup()
         self.arr.delete_by_id("77777")
@@ -170,11 +98,10 @@ class TestCollection(unittest.TestCase):
         caretaker.redo()
         caretaker.undo()
         caretaker.redo()
-        self.assertEqual(self.arr, temp)
+        self.assertNotIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"), self.arr)
         caretaker.undo()
-        self.arr.sort("currency",self.arr)
+        self.assertIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"),self.arr)
         caretaker.backup()
-        self.assertEqual(self.arr, unCorrect)
 
 if __name__ == "__main__":
     unittest.main()
