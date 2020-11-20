441785e41534c50a60d882830ca610bb5bbcaf73
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2020-11-20 20:24:43 +0200

    Extra lines of code were changed to cycles.

--- TestCollection.py
+++ TestCollection.py
@@ -1,14 +1,28 @@
 import unittest
+import copy
 from Collection6 import*
 
 class TestCollection(unittest.TestCase):
 
     def setUp(self):
+        #list from file
         self.arr = Collection()
         self.arr.add(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
         self.arr.add(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"))
         self.arr.add(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"))
 
+        #extra list for test_add/test_edit
+        self.extra = Collection()
+        self.extra.add(SwiftTransfer("00000", "UA00 0000 0000 0000", "Lomachenko Andriy", "12500.00", "usd", "565.00", "2010.01.11"))
+        self.extra.add(SwiftTransfer("11111", "UA11 1111 1111 1111", "Tetyana Stoyko", "11567.00", "uah", "153.00", "2019.02.02"))
+        self.extra.add(SwiftTransfer("22222", "UA22 2222 2222 2222", "Boyko Anatoliy", "22222.00", "eur", "557.00", "2018.02.27"))
+
+        #list with ids from the file
+        self.ListID = ["68292","67899","77777"]
+
+        #Caretaker is used in test_undo/test_redo
+        self.caretaker = Caretaker(self.arr)
+        self.caretaker.backup()
 
     def test_readFromFile(self):
         file = open("data.txt")
@@ -32,76 +46,53 @@ class TestCollection(unittest.TestCase):
         self.assertIsInstance(temp[0], SwiftTransfer)
 
     def test_find(self):
-        self.assertEqual(self.arr.search("Tor"),[self.arr[0]])
-        self.assertEqual(self.arr.search("eur"),[self.arr[0],self.arr[1],self.arr[2]])
-
-        self.assertNotEqual(self.arr.search("tor"),[self.arr[1]])
-        self.assertNotEqual(self.arr.search("7777"),[self.arr[1]])
+        for i in range(len(self.ListID)-1):
+            self.assertEqual(self.arr.search(self.ListID[i]), [self.arr[i]])
 
     def test_sort(self):
-        unCorrect = Collection()
-        for i in range(self.arr.len()):
-            unCorrect.add(self.arr[i])
+        unSorted = copy.deepcopy(self.arr)
 
         self.arr.sort("iban", self.arr)
         self.arr.sort("fee", self.arr)
-        self.assertEqual(self.arr,unCorrect)
+        self.assertEqual(self.arr,unSorted)
 
         self.arr.sort("currency", self.arr)
-        self.assertEqual(self.arr,unCorrect)
-
+        self.assertEqual(self.arr,unSorted)
 
     def test_delete(self):
-        self.arr.delete_by_id("77777")
-        self.assertNotIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"), self.arr)
-
-        self.arr.delete_by_id("67899")
-        self.assertNotIn(SwiftTransfer("67899", "UA88 8888 8888 8888", "Lomanow Ivan", "11000.00", "eur", "650.00", "2020.03.04"), self.arr)
+        for i in range(len(self.ListID)):
+            deletable = copy.deepcopy(self.arr[0])
+            self.arr.delete_by_id(self.ListID[i])
+            self.assertNotIn(deletable,self.arr)
 
     def test_add(self):
-        self.arr.add_inputed(self.arr, SwiftTransfer("55555", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"))
-        self.assertIn(SwiftTransfer("55555", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"), self.arr)
-
-        self.arr.add_inputed(self.arr, SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        self.assertIn(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"), self.arr)
-
-        self.arr.add_inputed(self.arr, SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"))
-        self.assertIn(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"), self.arr)
+        for i in range(self.extra.len()):
+            self.arr.add_inputed(self.arr, self.extra[i])
+            self.assertIn(self.extra[i],self.arr)
 
     def test_edit(self):
-        self.arr.edit_by_id("77777", self.arr, SwiftTransfer("77777", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"))
-        self.assertNotIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"),self.arr)
-        self.assertIn(SwiftTransfer("77777", "UA55 5555 5555 5555", "Ivan Boyko", "5555.00", "uah", "555.00", "2020.05.15"),self.arr)
-
-        self.arr.edit_by_id("68292", self.arr, SwiftTransfer("68292", "UA22 2222 2222 2222", "Toroniy Ivan", "30000.00", "usd", "600.00", "2018.09.17"))
-        self.assertNotIn(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"),self.arr)
-        self.assertIn(SwiftTransfer("68292", "UA22 2222 2222 2222", "Toroniy Ivan", "30000.00", "usd", "600.00", "2018.09.17"),self.arr)
+        for i in range(self.extra.len()):
+            changable = copy.deepcopy(self.arr[i])
+            self.arr.edit_by_id(self.ListID[i], self.arr, self.extra[i])
+            self.assertIn(self.extra[i],self.arr)
+            self.assertNotIn(changable,self.arr)
 
     def test_undo(self):
-        caretaker = Caretaker(self.arr)
-        caretaker.backup()
-        self.arr.delete_by_id("77777")
-        caretaker.backup()
-        caretaker.undo()
-        self.assertIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"),self.arr)
-        self.arr.delete_by_id("68292")
-        caretaker.backup()
-        caretaker.undo()
-        self.assertIn(SwiftTransfer("68292", "UA21 3546 8231 9022", "Toroniy Serhiy", "20000.00", "eur", "500.00", "2020.09.17"),self.arr)
+        for i in range(len(self.ListID)):
+            deletable = copy.deepcopy(self.arr[0])
+            self.arr.delete_by_id(self.ListID[i])
+            self.caretaker.backup()
+            self.caretaker.undo()
+            self.assertIn(deletable,self.arr)
 
     def test_redo(self):
-        caretaker = Caretaker(self.arr)
-        caretaker.backup()
-        self.arr.delete_by_id("77777")
-        caretaker.backup()
-        caretaker.undo()
-        caretaker.redo()
-        caretaker.undo()
-        caretaker.redo()
-        self.assertNotIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"), self.arr)
-        caretaker.undo()
-        self.assertIn(SwiftTransfer("77777", "UA77 7777 7777 7777", "seven s", "7777.00", "eur", "777.00", "2020.07.17"),self.arr)
-        caretaker.backup()
+        for i in range(len(self.ListID)):
+            deletable = copy.deepcopy(self.arr[0])
+            self.arr.delete_by_id(self.ListID[i])
+            self.caretaker.backup()
+            self.caretaker.undo()
+            self.caretaker.redo()
+            self.assertNotIn(deletable, self.arr)
 
 if __name__ == "__main__":
     unittest.main()
