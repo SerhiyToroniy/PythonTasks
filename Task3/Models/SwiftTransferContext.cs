3958672e6124186b4c47c7082c573a8ea6f6df51
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2021-03-15 13:10:29 +0200

    Add files via upload

---
+++ Task3/Models/SwiftTransferContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SwiftTransferAPI.Models
+{
+    public class SwiftTransferContext : DbContext
+    {
+        public SwiftTransferContext(DbContextOptions<SwiftTransferContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SwiftTransfer> SwiftTransfers { get; set; }
+    }
+}
